87f8c6656db055ef1e402746291332cd81a21309
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-05-22 12:09:31 -0300

    Add feature for new cards add

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const btnSubmit = document.querySelector('#submit');
 const btnAddBook = document.querySelector('#addBook');
 const formModal = document.querySelector('#form-modal');
 
-let mylibrary = [];
+let library = [
+	{
+		title: 'The Hobbit',
+		author: 'tolkien',
+		pages: 360,
+		isRead: true,
+	},
+	{
+		title: 'Sobre a brevidade da vida',
+		author: 'Sêneca',
+		pages: 120,
+		isRead: true,
+	},
+];
 
 class Book {
 	constructor(title, author, pages, isRead) {
@@ -13,7 +26,20 @@ class Book {
 	}
 }
 
-function addBook() {
+function makeNewCards() {
+	library.forEach((book) => makeCard(book));
+}
+
+function openFormModal() {
+	formModal.style.display = 'flex';
+}
+
+function closeFormModal() {
+	formModal.style.display = 'none';
+}
+
+function addBook(e) {
+	e.preventDefault();
 	let book = getInputValues();
 
 	if (book === false) {
@@ -21,8 +47,9 @@ function addBook() {
 		return;
 	}
 
-	mylibrary.push(book);
+	library.push(book);
 
+	makeCard(library[library.length - 1]);
 	closeFormModal();
 }
 
@@ -39,18 +66,101 @@ function getInputValues() {
 	return new Book(title, author, pages, isRead);
 }
 
-const openFormModal = () => {
-	formModal.style.display = 'flex';
-};
+function makeCard(book) {
+	let cardContainer = document.createElement('div');
+	let titleCotainer = document.createElement('div');
+	let authorContainer = document.createElement('div');
+	let pagesContainer = document.createElement('div');
 
-const closeFormModal = () => {
-	formModal.style.display = 'none';
-};
+	addClass(cardContainer, titleCotainer, authorContainer, pagesContainer);
+	setValues(book, titleCotainer, authorContainer, pagesContainer);
+	appendCardAtributtes(
+		cardContainer,
+		titleCotainer,
+		authorContainer,
+		pagesContainer
+	);
+	setButtons(book, cardContainer);
+	appendCard(cardContainer);
+	setDataIndex();
+}
+
+function addClass(card, title, author, pages) {
+	card.classList = 'card-container';
+	title.classList = 'book-title';
+	author.classList = 'book-author';
+	pages.classList = 'book-pages';
+}
+
+function setValues(book, title, author, pages) {
+	title.innerText = `${book.title}`;
+	author.innerText = `${book.author}`;
+	pages.innerText = `${book.pages} pages`;
+}
+
+function appendCardAtributtes(card, title, author, pages) {
+	card.appendChild(title);
+	card.appendChild(author);
+	card.appendChild(pages);
+}
+
+function setButtons(book, card) {
+	let btnRemove = document.createElement('button');
+	let btnRead = document.createElement('button');
+
+	btnRemove.classList = 'remove';
+	btnRemove.innerText = 'Remove';
+
+	toogleReadBtn(book, btnRead);
+	addfunctions(btnRead, btnRemove);
+	card.appendChild(btnRead);
+	card.appendChild(btnRemove);
+}
+
+function toogleReadBtn(book, btn) {
+	btn.classList = book.isRead ? 'read' : 'not-read';
+	btn.innerText = book.isRead ? 'Read' : 'Not Read';
+}
+
+function addfunctions(btnRead, btnRemove) {
+	btnRemove.addEventListener('click', removeCard);
+	btnRead.addEventListener('click', updadeBookReadStatus);
+}
+
+function removeCard(e) {
+	let card = e.target.parentElement;
+	removeBookFromLibrary(card.dataset.number);
+	card.remove();
+	setDataIndex();
+}
+
+function removeBookFromLibrary(book) {
+	library.splice(library[book], 1);
+}
+
+function updadeBookReadStatus(e) {
+	let index = e.target.parentElement.dataset.index;
+	library[index].isRead = library[index].isRead ? false : true;
+
+	toogleReadBtn(library[index], e.target);
+}
+
+function appendCard(card) {
+	let Cards = document.querySelector('.cards');
+	Cards.appendChild(card);
+}
+
+function setDataIndex() {
+	let cards = [...document.querySelectorAll('.card-container')];
+	cards.forEach((card) => {
+		card.dataset.index = cards.indexOf(card);
+	});
+}
 
 btnAddBook.addEventListener('click', openFormModal);
 formModal.addEventListener('click', (e) => {
 	if (e.target != formModal) return;
 	closeFormModal();
 });
-
 btnSubmit.addEventListener('click', addBook);
+makeNewCards();
